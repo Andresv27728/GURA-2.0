9f60b888c42afef408df42c57b886693b153f47b
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2026-05-31 18:20:52 -0500

    Update pago.js

--- cmds/YSY/pago.js
+++ cmds/YSY/pago.js
@@ -1,18 +1,14 @@
-// plugins/pago.js
-
-const handler = async (m, { conn }) => {
-  await conn.sendMessage(m.chat, {
-    requestPaymentMessage: {
-      amount: 50000,
-      currency: 'IDR',
-      note: 'Payment for order #123',
-      from: '573133374132@s.whatsapp.net'
-    }
-  })
+export default {
+  command: ['pago', 'payment'],
+  category: 'tools',
+  run: async ({ msg, sock }) => {
+    await sock.sendMessage(msg.chat, {
+      requestPaymentMessage: {
+        amount: 50000,
+        currency: 'IDR',
+        note: 'Payment for order #123',
+        from: '573133374132@s.whatsapp.net'
+      }
+    }, { quoted: msg })
+  }
 }
-
-handler.help = ['pago']
-handler.tags = ['tools']
-handler.command = /^(pago|payment)$/i
-
-export default handler
