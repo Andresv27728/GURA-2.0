4b6e2d571461f3741a53a634daf3848d8134232c
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2026-05-31 18:25:41 -0500

    Update pago.js

--- cmds/YSY/pago.js
+++ cmds/YSY/pago.js
@@ -4,9 +4,9 @@ export default {
   run: async ({ msg, sock }) => {
     await sock.sendMessage(msg.chat, {
       requestPaymentMessage: {
-        amount: 50000,
-        currency: 'IDR',
-        note: 'Payment for order #123',
+        amount: 15000,
+        currency: 'USD',
+        note: 'Pago por orden #123',
         from: '573133374132@s.whatsapp.net'
       }
     }, { quoted: msg })
