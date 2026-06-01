5fdfaa240b42c1edbf44584fea7a1e02ce627fe0
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2026-06-01 16:50:54 -0500

    Update restart.js

--- cmds/owner/restart.js
+++ cmds/owner/restart.js
@@ -4,13 +4,48 @@ export default {
   description: 'Reiniciar el socket del bot.',
   isOwner: true,
   run: async ({ msg, sock }) => {
-    await sock.reply(msg.chat, `✎ Reiniciando el Socket...\n> *Espere un momento...*`, msg);
-    setTimeout(() => {
-      if (process.send) {
-        process.send("restart");
-      } else {
-        process.exit(0);
-      }
-    }, 3000);
-  },
-};
+    try {
+      const baileys = await import('baileys')
+
+      const msg2 = baileys.generateWAMessageFromContent(
+        msg.chat,
+        baileys.proto.Message.fromObject({
+          interactiveMessage: {
+            header: {
+              title: ": ̗̀「𝐈𝐬𝐨𝐥𝐚𝐭𝐞𝐝𝐋𝐚𝐛𝐬」"
+            },
+            body: {
+              text: "✎ Reiniciando el Socket...\n> *Espere un momento...*"
+            },
+            nativeFlowMessage: {
+              buttons: [
+                {
+                  name: "inapp_signup",
+                  buttonParamsJson: "https://yosoyyo-api-ofc.onrender.com"
+                }
+              ]
+            }
+          }
+        }),
+        {}
+      )
+
+      await sock.relayMessage(
+        msg.chat,
+        msg2.message,
+        { messageId: msg2.key.id }
+      )
+
+      setTimeout(() => {
+        if (process.send) {
+          process.send("restart")
+        } else {
+          process.exit(0)
+        }
+      }, 3000)
+
+    } catch (e) {
+      return msg.reply(`> Ocurrió un error al ejecutar el comando.\n> [Error: *${e.message}*]`)
+    }
+  }
+}
