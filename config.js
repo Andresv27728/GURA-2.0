f2311bc16f5c4e052c397b171144fbad1a03c976
Andresv27728 <andrescamilovallejogomez701@gmail.com>
2026-06-03 23:19:01 -0500

    Update config.js

--- config.js
+++ config.js
@@ -6,3 +6,20 @@ export default {
     }
   }
 }
+
+
+  api: {
+    adonix: {
+      baseURL: "https://myapiadonix.casacam.net",
+      apiKey: "AdonixKeym3b8mi1061"
+    },
+    gemini: "AIzaSyDEww4IKqba9tgfb8ndMDBOoLkl-nSy4tw", // Tu API Key de Gemini
+    tiktok: "af11563abemshfa1b96b74c5d44ep1dcb7ajsnb5ab0d38ef96",
+    searchapi: "22f66JK9VLbTZVgC8chFAYRA",
+    githubToken: process.env.GITHUB_TOKEN,
+    githubRepo: process.env.GITHUB_REPO || "Andresv27728/bit.pf",
+    yosoyyo: {
+      baseURL: "https://yosoyyo-api-ofc.onrender.com/api",
+      apiKey: "Andresv27728"
+    }
+  },
